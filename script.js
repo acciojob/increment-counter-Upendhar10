70e3a38ad1491a4fe96b690c54d71bc8a66141c6
N.Upendhar <80301044+Upendhar10@users.noreply.github.com>
2025-07-09 22:11:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let counter = document.getElementById("counterVal");
+let counterBtn = document.getElementById("counterBtn");
+
+counterBtn.addEventListener("click", () => {
+	counterVal = parseInt(counter.innerText);
+	alert(counterVal);
+	counterVal = counterVal+1;
+	counter.innerText = counterVal;
+	// return counterVal;
+});
